af8858703c813c9c9dee80b77177dad64ee95518
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-03-27 12:20:15 +0200

    quest POO TypeScript1

---
+++ POO_TS1.ts
@@ -0,0 +1,26 @@
+class Person {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  tellMyName() {
+    console.log(`I am ${this.name}`);
+  }
+
+  tellMyAge() {
+    console.log(`I am ${this.age} years old`);
+  }
+}
+
+let person1 = new Person("John", 40);
+let person2 = new Person("Mary", 35);
+
+person1.tellMyName();
+person1.tellMyAge();
+
+person2.tellMyName();
+person2.tellMyAge();
